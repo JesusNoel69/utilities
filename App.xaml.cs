7776a14474e30bd2dcda407fa15a8ad4a174d4bf
Jesus <jnoel883@gmail.com>
2026-02-08 19:03:00 -0700

    Added first window

---
+++ App.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace utilities;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+        var secondWindow = new Window(new ContentPage())
+        {
+            Title = "Second window",
+            Width = 700,
+            Height = 500,
+            X = 100,
+            Y = 100
+        };
+
+        Current?.OpenWindow(secondWindow);
+
+	}
+    
+    
+
+	 protected override void OnStart()
+    {
+        base.OnStart();
+    }
+
+    protected override void OnResume()
+    {
+        base.OnResume();
+    }
+
+    protected override void OnSleep()
+    {
+        base.OnSleep();
+    }
+}
